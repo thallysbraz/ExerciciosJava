85a0d6fe3dd52cc6152c7ed4aa64810a5ff7be52
thallysbraz <thallysbraz3@gmail.com>
2020-02-13 13:50:10 -0300

    exercicios java

---
+++ javaAprendiz/src/app/Main.java
@@ -0,0 +1,26 @@
+package app;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number = 0;
+		number = soma(1, 4);
+
+		Scanner sc = new Scanner(System.in);
+		String s;
+
+		System.out.printf("Informe uma cadeia de caracteres:\n");
+		s = sc.nextLine();
+		System.out.println("Você digitou: " + s);
+
+		System.out.println("Hello Java Eclipse, number: " + number);
+	}
+
+	public static int soma(int number1, int number2) {
+		int sum = number1 + number2;
+
+		return sum;
+	}
+}
